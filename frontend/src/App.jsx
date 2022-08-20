3eea187fd6f3855d7478b584d11769514ea70782
jatin <jatinkachhawaha10@gmail.com>
2022-08-20 13:38:07 +0530

    tilwind and router install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
-export default function App(){
-    return(
-        <>
-        </>
-    )
+import React from 'react'
+import Top from './Components/Top'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+
+
+export default function App() {
+  return (
+    <>
+    <Top/>
+    <Navbar/>
+    <Banner/>
+    </>
+  )
 }
