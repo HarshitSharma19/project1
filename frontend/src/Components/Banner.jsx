d73af9fe8f3ab8785b953e202aaa97cb3be78c06
jatin <jatinkachhawaha10@gmail.com>
2022-08-30 12:10:47 +0530

    nabar change

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -3,19 +3,19 @@ import banner from '../Images/iphone_6_plus.svg';
 import Container from './Container';
 
 export default function Banner() {
-return (
+  return (
 
-<div
-  style={{background: "transparent linear-gradient(67deg, #E71D3A 0%, #ECC7C1 45%, #EFCAC4 58%, #E4BDB8 70%, #42A8FE 100%) 0% 0% no-repeat padding-box"}}>
-  <Container>
-    <div className='h-[400px] flex flex-row-reverse opacity-[1] '>
+    <div
+      style={{ background: "transparent linear-gradient(67deg, #E71D3A 0%, #ECC7C1 45%, #EFCAC4 58%, #E4BDB8 70%, #42A8FE 100%) 0% 0% no-repeat padding-box" }}>
+      <Container>
+        <div className='h-[400px] flex flex-row-reverse opacity-[1] '>
 
-      <img className='w-[570px] h-[100%] ' src={banner} alt="" />
+          <img className='w-[570px] h-[100%] ' src={banner} alt="" />
 
+
+        </div>
+      </Container>
 
     </div>
-  </Container>
-
-</div>
-)
-      }
+  )
+}
